e01836bc7fb85a0e2dd27824ab4a96aa5ebcbdf3
Vinay <u6776177@anu.edu.au>
2020-07-25 17:52:11 +1000

    Creating API controller

---
+++ BloodDonationApplication/Controllers/DCandidateController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BloodDonationApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DCandidateController : ControllerBase
+    {
+        private readonly DonationDBContext _context;
+
+        public DCandidateController(DonationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DCandidate
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DCandidates>>> GetDCandidates()
+        {
+            return await _context.dCandidates.ToListAsync();
+        }
+
+        // GET: api/DCandidate/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DCandidates>> GetDCandidate(int id)
+        {
+            var dCandidate = await _context.dCandidates.FindAsync(id);
+
+            if (dCandidate == null)
+            {
+                return NotFound();
+            }
+
+            return dCandidate;
+        }
+
+        // PUT: api/DCandidate/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDCandidate(int id, DCandidates dCandidate)
+        {
+            dCandidate.id = id;
+
+            _context.Entry(dCandidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DCandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DCandidate
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<DCandidates>> PostDCandidate(DCandidates dCandidate)
+        {
+            _context.dCandidates.Add(dCandidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDCandidate", new { id = dCandidate.id }, dCandidate);
+        }
+
+        // DELETE: api/DCandidate/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DCandidates>> DeleteDCandidate(int id)
+        {
+            var dCandidate = await _context.dCandidates.FindAsync(id);
+            if (dCandidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.dCandidates.Remove(dCandidate);
+            await _context.SaveChangesAsync();
+
+            return dCandidate;
+        }
+
+        private bool DCandidateExists(int id)
+        {
+            return _context.dCandidates.Any(e => e.id == id);
+        }
+    }
+}
