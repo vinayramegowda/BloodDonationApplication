f08e3ea1c5c39426e8aa29fed3bb57f66df935c5
Vinay <u6776177@anu.edu.au>
2020-07-25 15:54:19 +1000

    Add project files.

---
+++ BloodDonationApplication/Models/DonationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonationApplication.Models
+{
+    public class DonationDBContext:DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<DCandidates> dCandidates { get; set; }
+    }
+}
